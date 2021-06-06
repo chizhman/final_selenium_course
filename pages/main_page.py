fe2c45adf4cfd7602a8d9afeec2d59394e003b9f
KingDitch <chizh21@yandex.ru>
2021-06-06 05:20:07 +0300

    more convinient switch to page in code

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,12 +1,18 @@
 from .base_page import BasePage
-from selenium.webdriver.common.by import By
+from .locators import MainPageLocators, LoginPage
+import time
 
 
 class MainPage(BasePage):
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
-        login_link.click()
+        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        link.click()
+        # return LoginPage(browser=self.browser, url=self.browser.current_url)
 
     def should_be_login_link(self):
-        assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), "Login is not presented"
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login is not presented"
 
+    def should_be_login_page(self):
+        assert "login" in self.browser.current_url, "This is url not for login page"
+        assert self.is_element_present(*LoginPage.should_be_login_form(LoginPage)), "There is no login form on this page"
+        assert self.is_element_present(*LoginPage.should_be_register_form(LoginPage)), "There is no register form on this page"
