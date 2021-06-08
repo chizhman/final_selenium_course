ff747a183b521266dded2bed17dcb9a5b3fdbe36
KingDitch <chizh21@yandex.ru>
2021-06-08 19:57:41 +0300

    Css selectors are done easier

--- pages/locators.py
+++ pages/locators.py
@@ -1,6 +1,4 @@
 from selenium.webdriver.common.by import By
-from .base_page import BasePage
-
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
@@ -8,20 +6,6 @@ class MainPageLocators():
 
 
 class LoginPage(BasePage):
-    def should_be_login_page(self):
-        self.should_be_login_url()
-        self.should_be_login_form()
-        self.should_be_register_form()
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
 
-    def should_be_login_url(self):
-        assert True
-
-    def should_be_login_form(self):
-        LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
-        assert True
-        return LOGIN_FORM
-
-    def should_be_register_form(self):
-        REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
-        assert True
-        return REGISTER_FORM
--- pages/main_page.py
+++ pages/main_page.py
@@ -9,16 +9,6 @@ class MainPage(BasePage):
         link.click()
         # return LoginPage(browser=self.browser, url=self.browser.current_url)
 
-    def should_be_login_link(self):
-        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login is not presented"
-
-    def should_be_login_page(self):
-        assert "login" in self.browser.current_url, "This is url not for login page"
-        assert self.is_element_present(*LoginPage.should_be_login_form(LoginPage)), "There is no login form on this page"
-        assert self.is_element_present(*LoginPage.should_be_register_form(LoginPage)), "There is no register form on this page"
-
-
-
 
 # class MainPage(BasePage):
 #     def __init__(self, *args, **kwargs):
